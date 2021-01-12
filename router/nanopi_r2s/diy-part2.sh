1a0cb0937637841a55c1d8f51302fe23a4fff2e5
buddle-hub <74858773+buddle-hub@users.noreply.github.com>
2021-01-12 13:48:28 +0800

    Update diy-part2.sh

--- router/nanopi_r2s/diy-part2.sh
+++ router/nanopi_r2s/diy-part2.sh
@@ -8,7 +8,7 @@
 #========================================================================================================================
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.2.2）
-# sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 # Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
